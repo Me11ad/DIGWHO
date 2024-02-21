3c3bfa0dcd959c4aacc21d21b511ffa67cb79729
Me11ad <112916199+Me11ad@users.noreply.github.com>
2024-02-21 12:41:16 +0500

    Update program.py

--- program.py
+++ program.py
@@ -1,13 +1,16 @@
 import random
 
-num = random.randint(1, 100)
+
 print("Добро пожаловать в числовую угадайку!")
 print("Введите имя!")
-name = (input())
+name = input()
+rng = int(input("Введите максимальное число"))
+num = random.randint(1, rng)
+
 
 
 def is_valid(a):
-    return a.isdigit() and 1 <= int(a) <= 100
+    return a.isdigit() and 1 <= int(a) <= rng
 
 def checker():
     while True:
@@ -15,25 +18,27 @@ def checker():
         if is_valid(numb):
             return int(numb)
         else:
-            print("A может быть все-таки введем целое число от 1 до 100?")
+            print(f"A может быть все-таки введем целое число от 1 до {rng}?")
 
 def compare_nums():
-    print(f"Приветствую {name}! Введите число от 1 до 100!")
+    print(f"Приветствую {name}! Введите число от 1 до {rng}!")
+    count = 0
     while True:
         a = checker()
+        count += 1
         if a < num:
             print(f"{name}, ваше число меньше загаданного, попробуйте еще разок!")
         elif a > num:
             print(f"{name}, ваше число больше загаданного, попробуйте еще разок!")
         else:
-            print(f"Вы угадали, поздравляем {name}!")
+            print(f"Вы угадали за {count} попыток, поздравляем {name}!")
             
-            print("Вы хотите начать заново?(Д/Н)")
-            accept = (input())
+            print("Вы хотите начать заново?(д/н)")
+            accept = input()
             while True:
-                if accept == 'Д':
+                if accept == 'д':
                     compare_nums()
-                elif accept == 'Н':
+                elif accept == 'н':
                     print("Пока-пока")
                     break
                 break
